798b4dbd569174ba278e878dea85bcf0b238e6aa
martin <martinwang7963@gmail.com>
2016-02-22 10:07:51 +0800

    Create organizer's class: Tag,Startup,NewsLink

--- DjangoUnleash/superorganizer/organizer/models.py
+++ DjangoUnleash/superorganizer/organizer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Models):
+    pass
+
+class Startups(models.Models):
+    pass
+
+class NewsLink(models.Models):
+    pass
