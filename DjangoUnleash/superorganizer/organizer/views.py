63b9e701d5e77d46a72d1fcfaf481227f2b3ff4e
martinwang <martinwang7963@gmail.com>
2016-02-24 21:34:07 +0800

    Test DataBase with url and views,Add templates in blog and organizer

--- DjangoUnleash/superorganizer/organizer/views.py
+++ DjangoUnleash/superorganizer/organizer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Tag
 
-# Create your views here.
+def homepage(request):
+    tag_list = Tag.objects.all()
+    output = ', '.join([tag.name for tag in tag_list])
+    return HttpResponse(output)
