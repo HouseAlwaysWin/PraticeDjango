f7b3aa5bf52779cbbc744762c173ff860f54736a
martinwang <martinwang7963@gmail.com>
2016-02-27 19:59:06 +0800

    fix template bug and Create blog's post_detail and post_list method

--- DjangoUnleash/superorganizer/blog/urls.py
+++ DjangoUnleash/superorganizer/blog/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
-
-from .views import post_list
+from django.conf.urls import include, url
+from .views import (post_list,post_detail)
 
 urlpatterns = [
     url(r'^$',
         post_list,
-        name='blog_post_list'),]
+        name='blog_post_list'),
+    url(r'^(?P<year>\d{4})/'
+        r'(?P<month>\{1,2})/'
+        r'(?P<slug>[\w\-]+)/$',
+        post_detail,
+        name='blog_post_detail'),
+    ]
--- DjangoUnleash/superorganizer/blog/views.py
+++ DjangoUnleash/superorganizer/blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-from ..models import Post
+from django.shortcuts import (render,
+                              get_object_or_404)
+from .models import Post
 
-def blog_post(request):
+
+def post_list(request):
     return render(
         request,
         'blog/post_list.html',
         {'post_list':Post.objects.all()})
+
+def post_detail(request, year, month, slug):
+    post = get_object_or_404(
+            Post,
+            pub_date__year=year,
+            pub_date__month=month,
+            slug__iexact=slug)
+    return render(
+            request,
+            'blog/post_detail.html',
+            {'post':post})
