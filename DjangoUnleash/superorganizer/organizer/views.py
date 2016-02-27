8c3281c4233229f1365188d9ec62f238f9265786
martinwang <martinwang7963@gmail.com>
2016-02-27 09:01:01 +0800

    Use render instead of render_to_response

--- DjangoUnleash/superorganizer/organizer/views.py
+++ DjangoUnleash/superorganizer/organizer/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import (render_to_response,
+from django.shortcuts import (render,
                               get_object_or_404)
 
 from .models import Tag
 
 def homepage(request):
-    return render_to_response(
+    return render(
+        request,
         'organizer/tag_list.html',
         {'tag_list':Tag.object.all()})
 
 def tag_detail(request, slug):
     tag = get_object_or_404(
         slug__iexact=slug)
-    return render_to_response(
+    return render(
+        request,
         'organizer/tag_detail.html',
         {'tag':tag})
