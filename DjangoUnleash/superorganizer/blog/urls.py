67abad40c7f39345437120e514b01a11cdd03e8e
martin <martinwang7963@gmail.com>
2016-03-01 09:09:28 +0800

    Set attribute to CBV

--- DjangoUnleash/superorganizer/blog/urls.py
+++ DjangoUnleash/superorganizer/blog/urls.py
@@ -5,11 +5,13 @@ from .views import (PostList,
 urlpatterns = [
     url(r'^$',
         PostList.as_view(),
+        {'paretn_template':'base.html'},
         name='blog_post_list'),
     
     url(r'^(?P<year>\d{4})/'
         r'(?P<month>\{1,2})/'
         r'(?P<slug>[\w\-]+)/$',
         post_detail,
+        {'paretn_template':'base.html'},
         name='blog_post_detail'),
     ]
--- DjangoUnleash/superorganizer/blog/views.py
+++ DjangoUnleash/superorganizer/blog/views.py
@@ -6,20 +6,25 @@ from django.views.generic import View
 
 
 
-def post_detail(request, year, month, slug):
+def post_detail(request, year, month, slug, parent_template=None):
     post = get_object_or_404(
-            Post,
-            pub_date__year=year,
-            pub_date__month=month,
-            slug__iexact=slug)
+        Post,
+        pub_date__year=year,
+        pub_date__month=month,
+        slug__iexact=slug)
     return render(
-            request,
-            'blog/post_detail.html',
-            {'post':post})
+        request,
+        'blog/post_detail.html',
+        {'post':post},
+        {'parent_template':parent_template})
+
 class PostList(VIew):
+    template_name = 'blog/post_list.html'
     
-    def get(self, request):
+    
+    def get(self, request, parent_template=None):
         return render(
             request,
-            'blog/post_list.html',
-            {'post_list':Post.objects.all()})
+            self.template_name,
+            {'post_list':Post.objects.all()},
+            {'parent_template':parent_template})
