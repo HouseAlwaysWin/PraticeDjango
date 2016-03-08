91222fce2f8371a810d2eec1e904623f9d2fc84c
martin <martinwang7963@gmail.com>
2016-03-08 10:00:43 +0800

    Setting Email Contact All DOne

--- DjangoUnleash/superorganizer/contact/views.py
+++ DjangoUnleash/superorganizer/contact/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import (render,
+                              redirect)
+from django.contrib.messages import success
+from django.views.generic import View
 
-# Create your views here.
+class ContaceView(View):
+    form_class = ContactForm
+    template_name = 'contact/contact_form.html'
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            {'form':self.form_class()}
+            )
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            mail_sent = bound_form.send_mail()
+            if mail_sent:
+                success(
+                    request,
+                    'Email successfully sent')
+                return redirect('blog_post_list')
+        return render(
+            request,
+            self.template_name,
+            {'form':bound_form})
+                    
+                        
+
+
--- DjangoUnleash/superorganizer/superorganizer/urls.py
+++ DjangoUnleash/superorganizer/superorganizer/urls.py
@@ -17,12 +17,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from organizer import urls as organizer_urls
 from blog import urls as blog_urls
+from contact import urls as contact_urls
 from .views import redirect_root
+
 
 urlpatterns = [
     url(r'^$',redirect_root),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(organizer_urls)),
     url(r'^blog/', include(blog_urls)),
+    url(r'^contact/'.include(contact_urls)),
 
 ]
