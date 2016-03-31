12f70f0d41af4fbe1ad24b5859b8a5f58c06a5be
martin <martinwang7963@gmail.com>
2016-03-31 16:05:04 +0800

    Add User's registeration URL

--- DjangoUnleash/superorganizer/user/urls.py
+++ DjangoUnleash/superorganizer/user/urls.py
@@ -2,6 +2,23 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
+
+password_url = [
+    url(r'^change/$',
+        auth_views.password_change,
+        {'template_name':
+         'user/password_change_form.html',
+         'post_change_redirect':reverse_lazy(
+             'pwURL:pw_change_done')},
+        name='pw_change'),
+    
+    url('^change/done/$',
+        auth_views.password_change_done,
+        {'template_name':
+         'user/password_change_done.html'},
+        name='pw_change_done'),
+]
 
 urlpatterns = [
     url(r'^$',
@@ -18,4 +35,7 @@ urlpatterns = [
          'extra_context':{
              'form':AuthenticationForm}},
         name='logout'),
+    # User's registery url
+    url(r'^password/',
+        include(password_urls)),
     ]
