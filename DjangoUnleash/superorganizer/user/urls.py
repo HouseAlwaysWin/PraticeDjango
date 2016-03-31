ec32382b892b5d0e877f6a4436cd7b9fbac8f9d4
martin <martinwang7963@gmail.com>
2016-03-31 17:08:45 +0800

    Add reset URL for security

--- DjangoUnleash/superorganizer/user/urls.py
+++ DjangoUnleash/superorganizer/user/urls.py
@@ -13,11 +13,42 @@ password_urls = [
              'pwURL:pw_change_done')},
         name='pw_change'),
     
-    url('^change/done/$',
+    url(r'^change/done/$',
         auth_views.password_change_done,
         {'template_name':
          'user/password_change_done.html'},
         name='pw_change_done'),
+
+    url(r'^reset/$',
+        auth_views.password_reset,
+        {'template_name':
+         'user/password_reset_form.html',
+         'email_template_name':
+         'user/password_reset_email.html',
+         'subject_template_name':
+         'user/password_reset_subject.txt',
+         'post_reset_redirect':
+         reverse_lazy(
+             'pwURL:pw_reset_sent')},
+        name='pw_reset_start'),
+
+    url(r'^reset/sent/$',
+        auth_views.password_reset_done,
+        {'template_name':
+         'user/password_reset_sent.html'},
+        name='pw_reset_sent'),
+
+    url(r'^reset/'
+        r'(?P<uidb64>[0-9A-Za-z_\-]+)/'
+        r'(?P<token>[0-9A-Za-z]{1,13}'
+        r'-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,
+        {'template_name':
+         'user/password_reset_confirm.html',
+         'post_reset_redirect':
+         reverse_lazy(
+             'pwURL:pw_reset_complete')},
+        name='pw_reset_confirm'),
 ]
 
 urlpatterns = [
